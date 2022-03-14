bd36440d57bed4f64a5011f701ca3753c897a3e1
mfkrg <mfkrg@yandex.ru>
2022-03-14 13:48:31 +0300

    0.0.3 -Added mythril ingot -Added cobalt ingot -Added blood ingot

--- src/main/java/net/mfkrg/examplemod/ExampleMod.java
+++ src/main/java/net/mfkrg/examplemod/ExampleMod.java
@@ -1,14 +1,18 @@
 package net.mfkrg.examplemod;
 
 import net.fabricmc.api.ModInitializer;
+import net.mfkrg.examplemod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MOD_ID = "examplemod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+
+		ModItems.registerModItems();
+
 	}
 }
---
+++ src/main/java/net/mfkrg/examplemod/item/ModItems.java
@@ -0,0 +1,30 @@
+package net.mfkrg.examplemod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.mfkrg.examplemod.ExampleMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+
+public class ModItems {
+
+    public static final Item BLOOD_INGOT = registerItem("blood_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item COBALT_INGOT = registerItem("cobalt_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC).fireproof()));
+    public static final Item MYTHRIL_INGOT = registerItem("mythril_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC).fireproof()));
+
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registry.ITEM, new Identifier(ExampleMod.MOD_ID, name), item);
+    }
+
+
+    public static void registerModItems(){
+        ExampleMod.LOGGER.info("Registering Mod Items for " + ExampleMod.MOD_ID);
+    }
+
+}
